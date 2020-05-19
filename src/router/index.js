7b5881cc399d666f9393a43df57652fcd15cabcd
zqqiang <qiangzhaoqing@gmail.com>
2020-05-18 23:45:33 -0700

    add router an store

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      name: "register",
+      path: "/register",
+      component: () => import("@/views/Register"),
+    },
+  ],
+});
---
+++ src/views/Register.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>Register View</div>
+</teplate>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+    name: "clinicRegister",
+    data() {
+        return {
+            username: "",
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        onSubmit() {
+
+        }
+    }
+};
+
+</script>
