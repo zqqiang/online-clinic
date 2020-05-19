33f7b36f9806d21139fad317a4f7013138d881ff
Zhaoqing Qiang <qiangzhaoqing@gmail.com>
2020-05-18 23:54:01 -0700

    Register feature (#2)
    * create main js
    
    * add router an store
    
    * fix register view warning
    
    Co-authored-by: zqqiang <zqqiang@fortinet.com>

--- src/main.js
+++ src/main.js
@@ -4,22 +4,6 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
-import { CHECK_AUTH } from "./store/actions.type";
-import ApiService from "./common/api.service";
-import DateFilter from "./common/date.filter";
-import ErrorFilter from "./common/error.filter";
-
-Vue.config.productionTip = false;
-Vue.filter("date", DateFilter);
-Vue.filter("error", ErrorFilter);
-
-ApiService.init();
-
-// Ensure we checked auth before each page load.
-router.beforeEach((to, from, next) =>
-  Promise.all([store.dispatch(CHECK_AUTH)]).then(next)
-);
-
 new Vue({
   router,
   store,
---
+++ src/views/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Register View</div>
+</template>
+
+<script>
+export default {
+  name: "clinicRegister",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit() {}
+  }
+};
+</script>
