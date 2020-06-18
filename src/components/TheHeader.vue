fdaf19f61e6b9be9001829706563c05ff973d709
Zhaoqing Qiang <qiangzhaoqing@gmail.com>
2020-06-17 23:17:51 -0700

    change to bulma css framwork

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,42 +1,52 @@
 <template>
-  <nav class="navbar navbar-light">
-    <div class="container">
-      <router-link class="navbar-brand" :to="{ name: 'home' }"
-        >clinic</router-link
-      >
-      <ul class="nav navbar-nav pull-xs-right">
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            active-class="active"
-            exact
-            :to="{ name: 'home' }"
-            >Home</router-link
-          >
-        </li>
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            active-class="active"
-            exact
-            :to="{ name: 'login' }"
-          >
-            <i class="ion-compose"></i>Sign in
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link class="navbar-item" :to="{ name: 'home' }">
+          <span class="icon is-large has-text-info">
+            <i class="fas fa-2x fa-home"></i>
+          </span>
+        </router-link>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link class="navbar-item" exact :to="{ name: 'home' }">
+            <strong>Home</strong>
           </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            active-class="active"
-            exact
-            :to="{ name: 'register' }"
-          >
-            <i class="ion-compose"></i>Sign up
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link
+                class="button is-primary"
+                exact
+                :to="{ name: 'login' }"
+                >Sign in</router-link
+              >
+              <router-link
+                class="button is-light"
+                exact
+                :to="{ name: 'register' }"
+                >Sign up</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
